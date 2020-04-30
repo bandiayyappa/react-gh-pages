118104e598e8409815e3343f8d8506fcfd98bf90
ayyappa <bandiayyappa.ab@gmail.com>
2020-04-30 17:45:02 +0530

    Updates

---
+++ precache-manifest.0f34317e230b1e114a7d552bf53573c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9a420db4114167b15f17097ed17df1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9f1a04d898902d574fad",
+    "url": "/react-gh-pages/static/css/main.1be70e97.chunk.css"
+  },
+  {
+    "revision": "1a45c555737bd8b258ea",
+    "url": "/react-gh-pages/static/js/2.d9cdffc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.d9cdffc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f1a04d898902d574fad",
+    "url": "/react-gh-pages/static/js/main.f236c268.chunk.js"
+  },
+  {
+    "revision": "e3bbcf6a4c2925acbeb0",
+    "url": "/react-gh-pages/static/js/runtime-main.067920d2.js"
+  }
+]);
